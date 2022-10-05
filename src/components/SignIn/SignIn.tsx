128d02f9f13aa505bd79407b942957c7e1225406
James Plummer <plum_1990@hotmail.co.uk>
2022-10-05 18:31:21 +0100

    add disclaimer to signin to hopefully avoid phishing strikes

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -17,11 +17,24 @@ const SignIn: React.FC<Props> = ({ signInWithGoogle, setIsAuthenticated }) => {
       <section>
         <h2>Welcome back!</h2>
         <span>Definitely-not-Discord</span>
-
+        <br />
+        <p>
+          <strong>DISCLAIMER:</strong>
+          <br />
+          This website is not "Discord", or related in any way to the product
+          "Discord".
+        </p>
+        <br />
+        <p>
+          This website is a personal portfolio project which aims to demonstrate
+          a working understanding of WebRTC, and uses styling which closely
+          resembles the product "Discord" as a vehicle to achieve this.
+        </p>
+        <br />
+        <p>If you are concerned that this may be a scam then DO NOT log in.</p>
         <div />
         <p>Preset servers and channels</p>
         <p>Sign in with Google</p>
-
         <button onClick={handleSignIn}>
           <FcGoogle size="24px" />
           <h3>Sign in with Google</h3>
